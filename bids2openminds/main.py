7532b1b7c00ac928342256da1dbd42a9d12686d3
Peyman Najafi <najafe.peyman@gmail.com>
2024-04-22 15:32:15 +0200

    getting the file name by os.path.basename  (#31)
    * getting the name from IRI instead of path
    
    * using basename
    
    * deleting path_iri variable

--- bids2openminds/main.py
+++ bids2openminds/main.py
@@ -231,8 +231,8 @@ def create_file(layout_df, BIDS_path):
         data_types = None
         extension = file["extension"]
         path = file["path"]
-        name = path[path.rfind("/") + 1 :]
         iri=IRI(pathlib.Path(path).as_uri())
+        name=os.path.basename(path)
         hashes = file_hash(path)
         storage_size = file_storage_size(path)
         if pd.isna(file["subject"]):
