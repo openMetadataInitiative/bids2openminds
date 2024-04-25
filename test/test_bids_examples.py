c808048112f757ebe9ef0160bbdddef47ca9f2df
Peyman-N <najafe.peyman@gmail.com>
2024-04-25 02:24:56 +0200

    adopting the tests for click

--- test/test_bids_examples.py
+++ test/test_bids_examples.py
@@ -1,14 +1,17 @@
 import os
 import pytest
 from openminds import Collection
+from click.testing import CliRunner
 
-import bids2openminds.converter
+from bids2openminds.converter import convert
 
 example_dataset_labels = ("ds003", "ds000247", "eeg_cbm", "asl001")
 
 @pytest.mark.parametrize("dataset_label", example_dataset_labels)
 def test_example_datasets(dataset_label):
     test_dir = os.path.join("bids-examples", dataset_label)
-    bids2openminds.converter.convert(test_dir)
+    runner = CliRunner()
+    result = runner.invoke(convert, [test_dir])
+    assert result.exit_code == 0
     c = Collection()
     c.load(os.path.join(test_dir, "openminds.jsonld"))
