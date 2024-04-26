262feaa72e5e4b57966dbb9edea08c8d54ae64ae
Peyman Najafi <najafe.peyman@gmail.com>
2024-04-26 13:24:58 +0200

    Adopting click as command line interfaces (#30)
    * initial
    
    * fixing the toml
    
    * final touches on adopting click
    
    * deleting test elements
    
    * help for include_empty_properties
    
    * typo
    
    * default for convert
    
    * adopting the tests for click
    
    * creating convert_click
    
    * cleaning the converter file
    
    * adding new tests for click
    
    * final modifications in click

--- bids2openminds/converter.py
+++ bids2openminds/converter.py
@@ -1,23 +1,20 @@
 from warnings import warn
 from bids import BIDSLayout, BIDSValidator
 import os
+import click
 from . import main
 from . import utility
 from . import globals
 
-
-
-def convert(bids_dir, output_filename=None):
-
-    if not (os.path.isdir(bids_dir)):
+def convert(input_path, output_path=None, multiple_files=False, include_empty_properties=False):  
+    if not (os.path.isdir(input_path)):
         raise NotADirectoryError(
-            f"The input directory is not valid, you have specified {bids_dir} which is not a directory."
+            f"The input directory is not valid, you have specified {input_path} which is not a directory."
         )
+    # if not(BIDSValidator().is_bids(input_path)):
+    #  raise NotADirectoryError(f"The input directory is not valid, you have specified {input_path} which is not a BIDS directory.")
 
-    # if not(BIDSValidator().is_bids(bids_dir)):
-    #  raise NotADirectoryError(f"The input directory is not valid, you have specified {bids_dir} which is not a BIDS directory.")
-
-    bids_layout = BIDSLayout(bids_dir)
+    bids_layout = BIDSLayout(input_path)
 
     layout_df = bids_layout.to_df()
 
@@ -32,22 +29,40 @@ def convert(bids_dir, output_filename=None):
 
     [subjects_dict, subject_state_dict, subjects_list] = main.create_subjects(subjects_id, layout_df, bids_layout)
 
-    [files_list, file_repository] = main.create_file(layout_df, bids_dir)
+    [files_list, file_repository] = main.create_file(layout_df, input_path)
 
     dataset_version = main.create_dataset_version(
         bids_layout, dataset_description, layout_df, subjects_list, file_repository
     )
 
     dataset = main.create_dataset(dataset_description, dataset_version)
-
+    
     failures = globals.collection.validate(ignore=["required", "value"])
     assert len(failures) == 0
 
-    if output_filename is None:
-        output_filename = os.path.join(bids_dir, "openminds.jsonld")
-    globals.collection.save(output_filename)
+    if output_path is None:
+        if multiple_files:
+            output_path=os.path.join(input_path, "openminds")
+        else:
+            output_path = os.path.join(input_path, "openminds.jsonld")
+
+    globals.collection.save(output_path,individual_files=multiple_files,include_empty_properties=include_empty_properties)
+
+    print(f"Conversion was successful, the openMINDS file is in {output_path}")
+
+
+
+@click.command()
+@click.argument("input-path", type=click.Path(file_okay=False,exists=True))
+@click.option("-o","--output-path",default=None,type=click.Path(file_okay=True,writable=True),help="The output path or filename for OpenMINDS file/files.")
+@click.option("--single-file","multiple_files",flag_value=False,default=False,help="Save the entire collection into a single file (default).")
+@click.option("--multiple-files","multiple_files",flag_value=True,help="Each node is saved into a separate file within the specified directory. 'output-path' if specified, must be a directory.")
+@click.option("-e","--include-empty-properties",is_flag=True,default=False,help="Whether to include empty properties in the final file.")
+def convert_click(input_path, output_path, multiple_files, include_empty_properties):
+    convert(input_path, output_path, multiple_files, include_empty_properties)
+
 
 
 if __name__ == "__main__":
-    bids_dir = input("Enter the BIDS directory path: ")
-    convert(bids_dir)
+    input_path = input("Enter the BIDS directory path: ")
+    convert(input_path)
