afec789b2bfc6e3769eb44f816136ef2eecb9082
Peyman-N <najafe.peyman@gmail.Com>
2024-04-26 13:20:39 +0200

    final modifications in click

--- bids2openminds/converter.py
+++ bids2openminds/converter.py
@@ -6,7 +6,7 @@ from . import main
 from . import utility
 from . import globals
 
-def convert(input_path, output_path=None, output_separate_files=False, include_empty_properties=False):  
+def convert(input_path, output_path=None, multiple_files=False, include_empty_properties=False):  
     if not (os.path.isdir(input_path)):
         raise NotADirectoryError(
             f"The input directory is not valid, you have specified {input_path} which is not a directory."
@@ -41,25 +41,25 @@ def convert(input_path, output_path=None, output_separate_files=False, include_e
     assert len(failures) == 0
 
     if output_path is None:
-        if output_separate_files:
+        if multiple_files:
             output_path=os.path.join(input_path, "openminds")
         else:
             output_path = os.path.join(input_path, "openminds.jsonld")
 
-    globals.collection.save(output_path,individual_files=output_separate_files,include_empty_properties=include_empty_properties)
+    globals.collection.save(output_path,individual_files=multiple_files,include_empty_properties=include_empty_properties)
 
     print(f"Conversion was successful, the openMINDS file is in {output_path}")
 
 
 
 @click.command()
-@click.argument("input_path", type=click.Path(file_okay=False,exists=True))
-@click.option("-o","--output_path",default=None,type=click.Path(file_okay=True,writable=True),help="The output path or filename for OpenMINDS file/files.")
-@click.option("--single-file","output_separate_files",flag_value=True,default=False,help="Save the entire collection into a single file (default).")
-@click.option("--multiple-file","output_separate_files",flag_value=True,help="Each node is saved into a separate file within the specified directory. 'output_path' if specified, must be a directory.")
-@click.option("--include_empty_properties/--no-include_empty_properties",default=False,help="Whether to include empty properties in the final file.")
-def convert_click(input_path, output_path, output_separate_files, include_empty_properties):
-    convert(input_path, output_path, output_separate_files, include_empty_properties)
+@click.argument("input-path", type=click.Path(file_okay=False,exists=True))
+@click.option("-o","--output-path",default=None,type=click.Path(file_okay=True,writable=True),help="The output path or filename for OpenMINDS file/files.")
+@click.option("--single-file","multiple_files",flag_value=False,default=False,help="Save the entire collection into a single file (default).")
+@click.option("--multiple-files","multiple_files",flag_value=True,help="Each node is saved into a separate file within the specified directory. 'output-path' if specified, must be a directory.")
+@click.option("-e","--include-empty-properties",is_flag=True,default=False,help="Whether to include empty properties in the final file.")
+def convert_click(input_path, output_path, multiple_files, include_empty_properties):
+    convert(input_path, output_path, multiple_files, include_empty_properties)
 
 
 
