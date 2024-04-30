c8e02044393c857fa77835f316a5f57d00e0089e
Peyman-N <najafe.peyman@gmail.com>
2024-04-30 14:09:34 +0200

    correction for click test

--- test/test_example_datasets_click.py
+++ test/test_example_datasets_click.py
@@ -18,7 +18,7 @@ def test_example_datasets_click():
 def test_example_datasets_click_seperate_files():
     test_dir = os.path.join("bids-examples", test_data_set)
     runner = CliRunner()
-    result = runner.invoke(convert_click, ["--multiple-file", test_dir])
+    result = runner.invoke(convert_click, ["--multiple-files", test_dir])
     assert result.exit_code == 0
     path_openminds = os.path.join(test_dir, "openminds")
     numer_of_files = len(os.listdir(path_openminds))
@@ -27,9 +27,10 @@ def test_example_datasets_click_seperate_files():
 
 def test_example_datasets_click_output_location():
     test_dir = os.path.join("bids-examples", test_data_set)
+    openminds_file = os.path.join("bids-examples", "test_openminds.jsonld")
     runner = CliRunner()
     result = runner.invoke(
-        convert_click, ["-o", "test_openminds.jsonld", test_dir])
+        convert_click, ["-o", openminds_file, test_dir])
     assert result.exit_code == 0
     c = Collection()
-    c.load("test_openminds.jsonld")
+    c.load(openminds_file)
