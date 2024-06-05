38c39ee093506ff02194598c6cfa99001ef93958
Peyman-N <najafe.peyman@gmail.Com>
2024-06-05 14:44:09 +0200

    making report one object

--- bids2openminds/report.py
+++ bids2openminds/report.py
@@ -2,11 +2,6 @@ import os
 
 
 def create_report(dataset, dataset_version, collection, dataset_description, input_path):
-    print(f"Dataset title : {dataset.full_name}")
-    print("Authors : ")
-    for author in dataset_version.authors:
-        print(f"{author.given_name} {author.family_name}")
-
     subject_number = 0
     file_bundle_number = 0
     files_number = 0
@@ -21,20 +16,33 @@ def create_report(dataset, dataset_version, collection, dataset_description, inp
             case "https://openminds.ebrains.eu/core/File":
 
                 files_number += 1
+
 # TODO to be activated after merging the FileBundle pull request
 #            case "https://openminds.ebrains.eu/core/FileBundle":
 #
 #                file_bundle_number += 1
 
-    print("This dataset contains following items:")
-    print(f"Number of subjects : {subject_number}")
-    print(f"Number of files : {files_number}")
-#    print(f"Number of file bundles : {file_bundle_number}")
+    report = f"""Conversion Report
+=================  
 
+Dataset title : {dataset.full_name}
+
+The following elements were converted:  
+--------------------------------------  
++ number of converted subjects: {subject_number}  
++ number of state for each subject: XX-YY  
++ number of files: {files_number} 
++ number of files: {file_bundle_number}
+
+The following elements were not converted:  
+------------------------------------------  
+"""
     if "GeneratedBy" in dataset_description:
-        print("This dataset is derivative. The derivative section of BIDS specification is not final yet, so conversion is not complete. The generated openMINDS file should be treated carefully.")
+        report = report+"\n"+"+ Dataset is derivative, derivative data are ignored for now\n"
 
     derivatives_path = os.path.join(
         input_path, "derivatives")
     if os.path.isdir(derivatives_path):
-        print("This dataset contains derivative. The derivative section of BIDS specification is not final yet, so conversion is not complete. The generated openMINDS file should be treated carefully.")
+        report = report+"\n"+"+ Dataset contains derivative, derivative data are ignored for now\n"
+
+    print(report)
