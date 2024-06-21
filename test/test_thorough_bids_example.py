0cfcbb46a3b905d8bde35ed6335f3fd2f9862938
Peyman-N <najafe.peyman@gmail.Com>
2024-06-21 13:31:31 +0200

    adding file and fiel boundle

--- test/test_thorough_bids_example.py
+++ test/test_thorough_bids_example.py
@@ -3,7 +3,7 @@ import os
 from openminds import Collection
 import pytest
 
-dataset_labels = ["eeg_rest_fmri", "eeg_cbm"]
+dataset_labels = ["eeg_rest_fmri", "ds005"]
 
 
 @pytest.fixture(scope="session")
@@ -12,11 +12,18 @@ def load_collections():
     for dataset_label in dataset_labels:
         test_standard_name = "bids_examples_" + dataset_label + ".jsonld"
         test_standard_path = os.path.join("test", test_standard_name)
+
+        test_dataset = os.path.join("bids-examples", dataset_label)
+
+        with open(test_standard_path, "r+") as file:
+            undeisred_data = file.read()
+            desired_data = undeisred_data.replace("PREFIX", test_dataset)
+            file.write(desired_data)
+
         reference_collection = Collection()
         reference_collection.load(test_standard_path)
 
-        test_dataset = os.path.join("bids-examples", dataset_label)
-        bids2openminds.converter.convert(test_dataset)
+        bids2openminds.converter.convert(test_dataset, save_output=True)
         generated_collection = Collection()
         generated_collection.load(os.path.join(
             test_dataset, "openminds.jsonld"))
@@ -39,6 +46,13 @@ def detect_type(collection, type):
 def find_lookup_label(lookup_label, items):
     for item in items:
         if item.lookup_label == lookup_label:
+            return item
+    return None
+
+
+def find_name(name, items):
+    for item in items:
+        if item.name == name:
             return item
     return None
 
@@ -105,5 +119,39 @@ def test_dataset_version(load_collections, dataset_label):
     assert len(generated_dataset_version.experimental_approaches) == len(
         reference_dataset_version.experimental_approaches)
     assert generated_dataset_version.short_name == reference_dataset_version.short_name
-    assert len(generated_dataset_version.techniques) == len(
-        reference_dataset_version.techniques)
+    assert len(generated_dataset_version.techniques or []) == len(
+        reference_dataset_version.techniques or [])
+
+
+@pytest.mark.parametrize("dataset_label", dataset_labels)
+def test_file(load_collections, dataset_label):
+    reference_collection, generated_collection = load_collections[dataset_label]
+    generated_files = detect_type(generated_collection, "File")
+    reference_files = detect_type(reference_collection, "File")
+
+    assert len(generated_files) == len(reference_files)
+    for refrence_file in reference_files:
+        generated_file = find_name(refrence_file.name, generated_files)
+        assert generated_file is not None
+        assert generated_file.iri.value == refrence_file.iri.value
+        if generated_file.format is None:
+            assert refrence_file.format is None
+        else:
+            assert generated_file.format.id == refrence_file.format.id
+        assert generated_file.hashes[0].digest == refrence_file.hashes[0].digest
+
+
+@pytest.mark.parametrize("dataset_label", dataset_labels)
+def test_file_bundle(load_collections, dataset_label):
+    reference_collection, generated_collection = load_collections[dataset_label]
+    generated_file_bundles = detect_type(generated_collection, "FileBundle")
+    reference_file_bundles = detect_type(reference_collection, "FileBundle")
+
+    assert len(generated_file_bundles) == len(reference_file_bundles)
+    for refrence_file_bundle in reference_file_bundles:
+        generated_file_bundle = find_name(
+            refrence_file_bundle.name, generated_file_bundles)
+        assert generated_file_bundle is not None
+        if refrence_file_bundle.is_part_of.type_ == "https://openminds.ebrains.eu/core/FileBundle":
+            assert generated_file_bundle.is_part_of.type_ == "https://openminds.ebrains.eu/core/FileBundle"
+            assert refrence_file_bundle.is_part_of.name == generated_file_bundle.is_part_of.name
